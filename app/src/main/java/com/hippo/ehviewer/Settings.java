59cf1f992867eed9c78ab160637f131b446553c8
EVZYH <39973978+EVZYH@users.noreply.github.com>
2020-07-21 10:28:36 +0800

    修改5

--- app/src/main/java/com/hippo/ehviewer/Settings.java
+++ app/src/main/java/com/hippo/ehviewer/Settings.java
@@ -257,8 +257,8 @@ public class Settings {
     private static final boolean DEFAULT_GUIDE_GALLERY = true;
     private static final String KEY_CLIPBOARD_TEXT_HASH_CODE = "clipboard_text_hash_code";
     private static final int DEFAULT_CLIPBOARD_TEXT_HASH_CODE = 0;
-	private static final String KEY_DOWNLOAD_DELAY = "download_delay";
-	private static final int DEFAULT_DOWNLOAD_DELAY = 0;
+    private static final String KEY_DOWNLOAD_DELAY = "download_delay";
+    private static final int DEFAULT_DOWNLOAD_DELAY = 0;
     private static Context sContext;
     private static SharedPreferences sSettingsPre;
     private static EhConfig sEhConfig;
@@ -743,14 +743,14 @@ public class Settings {
     }
 
     public static int getDownloadDelay() {
-		return getIntFromStr(KEY_DOWNLOAD_DELAY, DEFAULT_DOWNLOAD_DELAY);
-	}
+        return getIntFromStr(KEY_DOWNLOAD_DELAY, DEFAULT_DOWNLOAD_DELAY);
+    }
 
-	public static void putDownloadDelay(int value) {
-		putIntToStr(KEY_DOWNLOAD_DELAY, value);
-	}
+    public static void putDownloadDelay(int value) {
+        putIntToStr(KEY_DOWNLOAD_DELAY, value);
+    }
 
-	public static int getPreloadImage() {
+    public static int getPreloadImage() {
         return getIntFromStr(KEY_PRELOAD_IMAGE, DEFAULT_PRELOAD_IMAGE);
     }
 
